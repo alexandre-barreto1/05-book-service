3499444aa979e090b2620e96c4c78202c134f56b
alexandre-dev <alexandre-dev@alexandre-dev>
2022-02-06 00:20:59 -0300

    Adicionando conexão book-service para cambio-service

--- src/main/java/br/com/erudio/controller/BookController.java
+++ src/main/java/br/com/erudio/controller/BookController.java
@@ -2,13 +2,16 @@ package br.com.erudio.controller;
 
 import br.com.erudio.model.Book;
 import br.com.erudio.repository.BookRepository;
+import br.com.erudio.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 @RestController
@@ -27,7 +30,20 @@ public class BookController {
         if(Objects.isNull(book)){
             throw new RuntimeException("book not found)");
         }
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().
+                getForEntity("http://localhost:8001/cambio-service/{amount}/{from}/{to}",
+                Cambio.class,
+                        params);
+
+        var cambio = response.getBody();
         var port = environment.getProperty("local.server.port");
+        book.setPrice(cambio.getConvertedValue());
         book.setEnviorment(port);
 
         return book;
